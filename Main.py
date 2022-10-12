fc66b08ebf15eb3c63fdecf23a82fb0a7e5d0e0f
neev17baptist <91770340+neev17baptist@users.noreply.github.com>
2022-10-12 19:45:31 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,23 @@
 from typing import List
+def parition(data, low, high) -> List[int]:
+    i = (low-1)
+    pivot = data[high]
+    for j in range(low, high):
+        if data[j] < pivot:
+            i = i+1
+            data[i], data[j] = data[j], data[i]
+    data[i+1], data[high] = data[high], data[i+1]
+    return (i+1)
 
-def insertionSort(array) -> List[int]:
-  # Write your code here
-  for i in range(1, len(array)):
-        k = array[i]
-        j = i-1
-        while j >= 0 and k < array[j] :
-                array[j + 1] = array[j]
-                j -= 1
-        array[j + 1] = k
-  return array  
+def quick_sort(data, low, high) -> List[int]:
+    # Write code here
+    if low < high:
+        pi = parition(data, low, high)
+        quick_sort(data, low, pi-1)
+        quick_sort(data, pi+1, high)
+    return data
 
-# data = [9, 5, 1, 4, 3]
+
 input_data = input()
 data = []
 for item in input_data.split(', '):
@@ -19,4 +25,4 @@ for item in input_data.split(', '):
     data.append(int(item))
   elif item.lstrip("-").isnumeric():
     data.append(int(item))
-print(insertionSort(data))
+print(quick_sort(data, 0, len(data)-1))
