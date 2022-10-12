996871eb2a9f98d5cf6b8fa0257fbe79ab272c31
neev17baptist <91770340+neev17baptist@users.noreply.github.com>
2022-10-12 18:44:31 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,17 @@
 from typing import List
 
-def quick_sort(data, low, high) -> List[int]:
-    # Write code here
+def insertionSort(array) -> List[int]:
+  # Write your code here
+  for i in range(1, len(array)):
+        k = array[i]
+        j = i-1
+        while j >= 0 and k < array[j] :
+                array[j + 1] = array[j]
+                j -= 1
+        array[j + 1] = k
+  return array  
 
-
+# data = [9, 5, 1, 4, 3]
 input_data = input()
 data = []
 for item in input_data.split(', '):
@@ -11,4 +19,4 @@ for item in input_data.split(', '):
     data.append(int(item))
   elif item.lstrip("-").isnumeric():
     data.append(int(item))
-print(quick_sort(data, 0, len(data)-1))
+print(insertionSort(data))
